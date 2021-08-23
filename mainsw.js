89be498fcf4e957c560469bbc9384e4ef387791f
Trygve Aaberge <trygveaa@gmail.com>
2021-08-23 23:54:34 +0200

    Add option for choosing if it should switch to tabs in the same window

--- mainsw.js
+++ mainsw.js
@@ -18,6 +18,7 @@ var timer;
 var slowswitchForward = false;
 
 var initialized = false;
+var options = {};
 
 var loggingOn = false;
 
@@ -130,13 +131,13 @@ var doIntSwitch = function() {
 		}
 		chrome.tabs.query({lastFocusedWindow: true, active: true}, function(activeTabs) {
 			var activeTab = activeTabs[0];
-			if (!activeTab) {
+			if (options.onlySameWindow && !activeTab) {
 				return;
 			}
 			tabIdToMakeActive = mru[intSwitchCount];
 			chrome.tabs.get(tabIdToMakeActive, function(tab) {
 				if(tab) {
-					if (tab.windowId !== activeTab.windowId || tab.id === activeTab.id) {
+					if (options.onlySameWindow && (tab.windowId !== activeTab.windowId || tab.id === activeTab.id)) {
 						doIntSwitch();
 					} else {
 						thisWindowId = tab.windowId;
@@ -260,6 +261,18 @@ var initialize = function() {
 				});
 			});
 			CLUTlog("MRU after init: "+mru);
+		});
+
+		chrome.storage.sync.get({
+			onlySameWindow: false
+		}, function(items) {
+			options.onlySameWindow = items.onlySameWindow;
+		});
+
+		chrome.storage.onChanged.addListener(function (changes) {
+			for (let [key, { newValue }] of Object.entries(changes)) {
+				options[key] = newValue;
+			}
 		});
 	}
 }	
